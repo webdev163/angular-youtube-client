f76e92068b840322e6f307484f8ba3cb1d585d81
Dmitry Kolesnichenko <dmitriykol@gmail.com>
2023-09-19 15:51:42 +0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NewCardComponent } from './core/new-card/new-card.component';
+import { FiltersComponent } from './core/filters/filters.component';
+import { CardDetailsComponent } from './search/card-details/card-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    RegistrationComponent,
+    LoginComponent,
+    NewCardComponent,
+    FiltersComponent,
+    CardDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/new-card/new-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-card',
+  templateUrl: './new-card.component.html',
+  styleUrls: ['./new-card.component.scss']
+})
+export class NewCardComponent {
+
+}
