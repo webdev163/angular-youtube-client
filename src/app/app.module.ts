7e5d389792f762e450f5a7aa14ce32e00dfce128
Dmitry Kolesnichenko <dmitriykol@gmail.com>
2023-10-02 15:26:32 +0400

    fix eslint errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { YoutubeModule } from './youtube/youtube.module';
 
 import { AppComponent } from './app.component';
 
