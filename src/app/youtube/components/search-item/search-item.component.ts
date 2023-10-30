71ae1871fd890e60e3b32743905f139a994b08fb
Dmitry Kolesnichenko <dmitriykol@gmail.com>
2023-10-30 19:24:00 +0400

    fix video item links

--- src/app/youtube/components/search-item/search-item.component.ts
+++ src/app/youtube/components/search-item/search-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SearchItem } from 'src/app/shared/models/search-item.model';
 import { CustomItem } from '~/shared/models/custom-item.model';
 
@@ -7,9 +7,13 @@ import { CustomItem } from '~/shared/models/custom-item.model';
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent {
+export class SearchItemComponent implements OnInit {
   @Input() data!: SearchItem;
   @Input() customData!: CustomItem;
 
-  public id = this.data?.id as string;
+  public id = '';
+
+  ngOnInit(): void {
+    if (this.data) this.id = this.data.id as string;
+  }
 }
